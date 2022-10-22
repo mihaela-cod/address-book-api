4bf1b12ddd7911a2b3b30d8f0eb9e129e52337b4
Mihaela Codrescu <mihaela_cod@yahoo.com>
2022-10-22 21:50:05 +0300

    add contact-data

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var teamsDBRouter = require("./routes/teams-db");
 var teamsRouter = require("./routes/teams-json");
+var teamsRouter = require("./routes/contact-data");
 
 var app = express();
 
@@ -34,7 +35,8 @@ function processingSimulate(req, res, next) {
     next();
   }, wait);
 }
-app.use("/teams-json", processingSimulate, teamsRouter);
+app.use("/contact-data", processingSimulate, teamsRouter);
+//app.use("/teams-json", processingSimulate, teamsRouter);
 //app.use("/teams-json", teamsRouter);
 
 // catch 404 and forward to error handler
